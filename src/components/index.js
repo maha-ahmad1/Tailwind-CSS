121952e2af72b0c591cf6b3e1175b5711a10d08d
maha-ahmad1 <maha.ahmad.k@hotmail.com>
2023-01-07 02:13:17 +0200

    tailwind css

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from "./navbar/Navbar";
+export { default as Hero } from "./hero/Hero";
+export { default as About } from "./about/About";
+export { default as Support } from "./support/Support";
+export { default as AllInOne } from "./allInone/AllInOne";
+export { default as Pricing } from "./pricing/Pricing";
+export { default as Footer } from "./footer/Footer";
